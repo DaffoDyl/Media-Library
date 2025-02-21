ed42a98b9f2fe5ff8ba3fce0dde9d2168b739c50
Dylan Armstrong <dylan.armstrong299@gmail.com>
2025-02-21 06:22:46 -0700

    Add media counts to home screen

--- app/src/main/java/com/daffodyl/medialibrary/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/daffodyl/medialibrary/ui/screens/HomeScreen.kt
@@ -7,17 +7,36 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewmodel.MutableCreationExtras
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.daffodyl.medialibrary.MediaLibraryApplication
 import com.daffodyl.medialibrary.ui.components.MediaBox
+import com.daffodyl.medialibrary.viewmodels.BooksScreenViewModel
+import com.daffodyl.medialibrary.viewmodels.HomeScreenViewModel
 
 @Composable
 fun HomeScreen(
     goToBoardGames: () -> Unit,
     goToBooks: () -> Unit,
     goToMovies: () -> Unit,
-    goToVideoGames: () -> Unit
+    goToVideoGames: () -> Unit,
+    viewModel: HomeScreenViewModel = viewModel(
+        factory = HomeScreenViewModel.Factory,
+        extras = MutableCreationExtras().apply {
+            this[APPLICATION_KEY] = LocalContext.current.applicationContext as MediaLibraryApplication
+        }
+    )
 ) {
+    val books by viewModel.books.collectAsState()
+    val movies by viewModel.movies.collectAsState()
+    val boardGames by viewModel.boardGames.collectAsState()
+    val videoGames by viewModel.videoGames.collectAsState()
     Column(
         modifier = Modifier
             .padding(16.dp)
@@ -28,14 +47,14 @@ fun HomeScreen(
         Row(
             horizontalArrangement = Arrangement.spacedBy(16.dp),
         ) {
-            MediaBox(modifier, "Books", goToBooks)
-            MediaBox(modifier, "Movies", goToMovies)
+            MediaBox(modifier, "Books (" + books.size + ")", goToBooks)
+            MediaBox(modifier, "Movies(" + movies.size + ")", goToMovies)
         }
         Row(
             horizontalArrangement = Arrangement.spacedBy(16.dp),
         ) {
-            MediaBox(modifier, "Board Games", goToBoardGames)
-            MediaBox(modifier, "Video Games", goToVideoGames)
+            MediaBox(modifier, "Board Games (" + boardGames.size + ")", goToBoardGames)
+            MediaBox(modifier, "Video Games (" + videoGames.size + ")", goToVideoGames)
         }
     }
 }
--- app/src/main/java/com/daffodyl/medialibrary/viewmodels/HomeScreenViewModel.kt
+++ app/src/main/java/com/daffodyl/medialibrary/viewmodels/HomeScreenViewModel.kt
@@ -1,4 +1,74 @@
 package com.daffodyl.medialibrary.viewmodels
 
-class HomeScreenViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.daffodyl.medialibrary.MediaLibraryApplication
+import com.daffodyl.medialibrary.models.BoardGame
+import com.daffodyl.medialibrary.models.Book
+import com.daffodyl.medialibrary.models.Movie
+import com.daffodyl.medialibrary.models.VideoGame
+import com.daffodyl.medialibrary.repositories.BoardGamesRepository
+import com.daffodyl.medialibrary.repositories.BooksRepository
+import com.daffodyl.medialibrary.repositories.MoviesRepository
+import com.daffodyl.medialibrary.repositories.VideoGamesRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class HomeScreenViewModel(
+    private val boardGamesRepository: BoardGamesRepository,
+    private val booksRepository: BooksRepository,
+    private val moviesRepository: MoviesRepository,
+    private val videoGamesRepository: VideoGamesRepository
+): ViewModel() {
+    private val _boardGames = MutableStateFlow(emptyList<BoardGame>())
+    private val _books = MutableStateFlow(emptyList<Book>())
+    private val _movies = MutableStateFlow(emptyList<Movie>())
+    private val _videoGames = MutableStateFlow(emptyList<VideoGame>())
+
+    val boardGames: StateFlow<List<BoardGame>> = _boardGames
+    val books: StateFlow<List<Book>> = _books
+    val movies: StateFlow<List<Movie>> = _movies
+    val videoGames: StateFlow<List<VideoGame>> = _videoGames
+
+    init {
+        viewModelScope.launch(Dispatchers.Default) {
+            boardGamesRepository.boardGames.collect {
+                _boardGames.value = it
+            }
+        }
+        viewModelScope.launch(Dispatchers.Default) {
+            booksRepository.books.collect {
+                _books.value = it
+            }
+        }
+        viewModelScope.launch(Dispatchers.Default) {
+            moviesRepository.movies.collect {
+                _movies.value = it
+            }
+        }
+        viewModelScope.launch(Dispatchers.Default) {
+            videoGamesRepository.videoGames.collect {
+                _videoGames.value = it
+            }
+        }
+    }
+
+    companion object {
+        val Factory = viewModelFactory {
+            initializer {
+                val application = this[APPLICATION_KEY] as MediaLibraryApplication
+                HomeScreenViewModel(
+                    application.boardGamesRepository,
+                    application.booksRepository,
+                    application.moviesRepository,
+                    application.videoGamesRepository,
+                )
+            }
+        }
+    }
 }
