787913228dff55f96843d2d1b06063f44c309049
Dylan Armstrong <dylan.armstrong299@gmail.com>
2025-02-28 20:09:28 -0700

    Remove import

--- app/src/main/java/com/daffodyl/medialibrary/ui/screens/CreateMovieScreen.kt
+++ app/src/main/java/com/daffodyl/medialibrary/ui/screens/CreateMovieScreen.kt
@@ -18,7 +18,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
 import androidx.lifecycle.viewmodel.MutableCreationExtras
 import androidx.lifecycle.viewmodel.compose.viewModel
